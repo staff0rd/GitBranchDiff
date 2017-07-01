9e0be4fadf896ecdeea948a1c03dfd409395f2c2
stafford <stafford.williams@gmail.com>
2017-07-01 15:13:49 +1000

    styling

--- GitBranchDiff/Model/Item.cs
+++ GitBranchDiff/Model/Item.cs
@@ -10,5 +10,7 @@ namespace GitBranchDiff.Model
     {
         public string Name { get; set; }
         public string Path { get; set; }
+        public bool IsDeleted { get; set; }
+        public bool IsAdded { get; set; }
     }
 }
