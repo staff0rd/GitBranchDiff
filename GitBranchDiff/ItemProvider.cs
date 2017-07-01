3e6513dae500ee3b26bb9a55ff865f26dd5a13b3
stafford <stafford.williams@gmail.com>
2017-07-01 15:12:54 +1000

    include current changes

--- GitBranchDiff/ItemProvider.cs
+++ GitBranchDiff/ItemProvider.cs
@@ -15,27 +15,55 @@ namespace GitBranchDiff
     {
         public List<Item> GetItems(string gitRoot, string branch)
         {
-            var changes = new ShellCommand().Execute("git", $"diff --name-only {branch}", gitRoot);
-            return MakeTreeFromPaths(changes);
+            if (branch.Contains("*"))
+                branch = branch.Replace("*", "").Trim();
+            var diff = new ShellCommand().Execute("git", $"diff --name-status {branch}", gitRoot);
+
+            var changes = new ShellCommand().Execute("git", "status --porcelain -u", gitRoot);
+
+            diff.AddRange(ConvertChanges(changes));
+
+            return MakeTreeFromPaths(diff);
         }
 
-        public List<Item> MakeTreeFromPaths(List<string> paths, string rootNodeName = "", char separator = '/')
+        public List<string> ConvertChanges(List<string> changes)
+        {
+            var result = new List<string>();
+            foreach(var change in changes)
+            {
+                var path = change.Substring(3);
+                switch (change.Substring(0, 2))
+                {
+                    case "??":
+                    case "A ": result.Add($"A\t{path}"); break;
+                    case "D ": result.Add($"D\t{path}"); break;
+                    case "M ": result.Add($"M\t{path}"); break;
+                }
+            }
+            return result;
+        }
+
+        public List<Item> MakeTreeFromPaths(List<string> changes, string rootNodeName = "", char separator = '/')
         {
             var rootNode = new DirectoryItem();
-            foreach (var path in paths.Where(x => !string.IsNullOrEmpty(x.Trim())))
+            foreach (var change in changes.Where(x => !string.IsNullOrEmpty(x.Trim())))
             {
+                var split = change.Split('\t');
+                var action = split[0];
+                var path = split[1];
+
                 var currentNode = rootNode;
                 var pathItems = path.Split(separator);
 
                 for (int i = 0; i < pathItems.Count(); i++) 
                 {
                     var item = pathItems[i];
-                    var tmp = currentNode.Items.Where(x => x.Name.Equals(item));
-                    if (tmp.Count() == 0)
+                    var tmp = currentNode.Items.FirstOrDefault(x => x.Name.Equals(item));
+                    if (tmp == null)
                     {
                         if (i == pathItems.Count() - 1)
                         {
-                            var newFile = new FileItem { Name = item, Path = path };
+                            var newFile = new FileItem { Name = item, Path = path, IsAdded = action == "A", IsDeleted = action == "D" };
                             currentNode.Items.Add(newFile);
                         } else
                         {
@@ -43,6 +71,9 @@ namespace GitBranchDiff
                             currentNode.Items.Add(newDirectory);
                             currentNode = newDirectory;
                         }
+                    } else if (tmp.GetType() == typeof(DirectoryItem))
+                    {
+                        currentNode = (DirectoryItem)tmp;
                     }
                 }
             }
