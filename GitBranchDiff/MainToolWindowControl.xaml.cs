3f1c094cbb599e80253c59f75623cec3b1bb2acd
Stafford Williams <stafford.williams@gmail.com>
2017-07-03 19:24:33 +1000

    remove unused code

--- GitBranchDiff/MainToolWindowControl.xaml.cs
+++ GitBranchDiff/MainToolWindowControl.xaml.cs
@@ -7,7 +7,6 @@
 namespace GitBranchDiff
 {
     using Model;
-    using System.Diagnostics.CodeAnalysis;
     using System.Windows;
     using System.Windows.Controls;
 
@@ -22,20 +21,6 @@ namespace GitBranchDiff
         public MainToolWindowControl()
         {
             this.InitializeComponent();
-        }
-
-        /// <summary>
-        /// Handles click on the button by displaying a message box.
-        /// </summary>
-        /// <param name="sender">The event sender.</param>
-        /// <param name="e">The event args.</param>
-        [SuppressMessage("Microsoft.Globalization", "CA1300:SpecifyMessageBoxOptions", Justification = "Sample code")]
-        [SuppressMessage("StyleCop.CSharp.NamingRules", "SA1300:ElementMustBeginWithUpperCaseLetter", Justification = "Default event handler naming pattern")]
-        private void button1_Click(object sender, RoutedEventArgs e)
-        {
-            MessageBox.Show(
-                string.Format(System.Globalization.CultureInfo.CurrentUICulture, "Invoked '{0}'", this.ToString()),
-                "MainToolWindow");
         }
 
         private void OnItemMouseDoubleClick(object sender, RoutedEventArgs e)
